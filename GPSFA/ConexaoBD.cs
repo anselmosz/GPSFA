a4b19f50593b0c79d99b79c274d5aa0756549d64
anselmosz <anselmoma2005@outlook.com>
2025-05-21 11:52:59 -0300

    update

---
+++ GPSFA/ConexaoBD.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace GPSFA
+{
+    public class ConexaoBD
+    {
+        private static string connString = "Server=localhost;Port=3306;Database=dbgpfsa;Uid=root;Pwd=''";
+
+        private static MySqlConnection conn = null;
+
+        public static MySqlConnection abrirConexao()
+        {
+            conn = new MySqlConnection(connString);
+
+            try
+            {
+                conn.Open();
+            }
+            catch (MySqlException)
+            {
+                return conn = null;
+            }
+            return conn;
+        }
+
+        public static void fecharConexao()
+        {
+            if (conn != null)
+            {
+                conn.Close();
+            }
+        }
+    }
+}
